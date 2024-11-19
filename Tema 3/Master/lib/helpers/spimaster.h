73c872cbb4e7d73952e6b3e43e402d694221c760
CristianPreda1609 <120663023+CristianPreda1609@users.noreply.github.com>
2024-11-20 00:37:52 +0200

    Delete Tema 3 directory

--- Tema 3/Master/lib/helpers/spimaster.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-void initSPI();
-
-void stopRound();
-
-void sendRoundState(bool, uint8_t);
-
-uint8_t pollSlave();
--- Tema 3/lib/spicom/spicom.h
+++
@@ -1,6 +0,0 @@
-// General
-#define LED_MASK 0x03
-
-// Master
-#define POLL_PIN 7
-#define PLAYER_PIN 6
